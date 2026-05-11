041567e2e2458652317514a542e42a04eb4311b9
0xfreddy <freddy0x@proton.me>
2026-05-11 21:30:57 +0700

    Initial Raycast extension

---
+++ raycast-env.d.ts
@@ -0,0 +1,29 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {
+  /** Favorite Topics - Comma-separated topics or teams to show first, for example Sports, NBA, Lakers. */
+  "favoriteTopics"?: string,
+  /** Default Market Status - Market status used when loading markets. */
+  "defaultStatus": "open" | "closed" | "settled"
+}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `search-markets` command */
+  export type SearchMarkets = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `search-markets` command */
+  export type SearchMarkets = {}
+}
+
